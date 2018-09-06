791f7e491c6ca27aa4b976424f02b4da7034e5a1
Vaibhav Tripathi <tripathivaibhav140@gmail.com>
2018-09-07 02:41:38 +0530

    Royale API used

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const request = require('request');
 const express= require('express');
 const hbs = require('hbs');
+const { Client } = require('pg');
 
 const port = process.env.PORT || 3000;
+
+/*const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+client.connect();*/
+
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
-
-const fixieRequest = request.defaults({'proxy': process.env.FIXIE_URL});
-
 app.set('view engine','hbs');
 
 /*app.use((req,res,next) =>{
@@ -17,10 +22,10 @@ app.set('view engine','hbs');
 
 
 var options = {
-	url : 'https://api.clashroyale.com/v1/clans/%23C2VQCU/members'
+	method : 'GET',
+	url : 'https://api.royaleapi.com/clan/C2VQCU'
 	,headers : {
-		Accept : 'application/json'
-		, authorization : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ0MjczNGFhLTdlNDUtNGM4NC1hOWE1LTNmOGQ2ZGM5ODdiYSIsImlhdCI6MTUzNjA4NTkyNCwic3ViIjoiZGV2ZWxvcGVyL2Q5OTc3YzdiLTEzMmQtODRlOC1kZjY1LWE4NDc1MmQzYzY2ZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0Ny45LjE0My4zNiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.o5wL60Xdch5I4BOVkAYAMAtEAHdsjrKHo5XtFWJgeez6wu10PCaw5sdOLGWFf_p8f1ObMBLyY9wNUjvwRNVt0A'
+		auth : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTYyOCwiaWRlbiI6IjI1NTcyODU2ODA2MTkxOTI0MyIsIm1kIjp7fSwidHMiOjE1MzYyMjc3MDE3MTh9.8tb3YdGuJxSDxZu_aO9aRSMSnwY1Y5st6srZqe3LyNg'
 	}
 	, json : true
 
@@ -29,16 +34,39 @@ var options = {
 app.get('/update', (req,res) =>{
 	request(options, (error,response,body) =>{
 	if(error)
-		console.log("error",error);
+		res.send(error);
 	else if(response.statusCode !== 200){
-		console.log(statusCode);
+		res.send(statusCode);
 	}
 	else{
 		console.log(JSON.stringify(body,undefined,2));
+		res.send(body);
 	}
+	//res.send('done');
+	
 
 });
 });
+
+app.get('/view',(req,res) =>{
+	
+	queryAll(res);
+	//res.send(
+});
+
+var queryAll= (res1) =>{
+	client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+		if (err) res1.render('error.hbs');
+		for (let row of res.rows) {
+			console.log(JSON.stringify(row));
+		}
+		res1.render(res.rows);
+	client.end();
+	});
+}
+
+
+
 
 app.get('/', (req,res) =>{
 	res.render('home.hbs',{} );
